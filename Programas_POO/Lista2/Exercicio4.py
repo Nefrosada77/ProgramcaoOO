bc2bb2c33dfa45947cab00702ecb617bb84b5465
Gabriel Siciliani <bibielsmo@gmail.com>
2023-10-18 17:36:40 -0300

    Update exercicio 4 lista 2

--- Programas_POO/Lista2/Exercicio4.py
+++ Programas_POO/Lista2/Exercicio4.py
@@ -1,5 +1,6 @@
 import random
 import os
+from termcolor import colored
 class dado:
     def __init__(self,lados) -> None:
         self.lados = lados
@@ -18,73 +19,50 @@ class Competidor:
     
     def atualizar(self,dado):
         self.pos += dado
-        if (self.pos % 5) == 0:
-            print(self.nome,"Derrapa na curva!!")
+        if (self.pos % 5) == 0 and self.pos < 20:
+            print(colored(f"{self.nome} derrapa na curva!!","red"))
             self.pos -= 1
         elif self.pos == 7 or self.pos == 17:
-            print(self.nome,"Acelera com tudo!!")
+            print(colored(f"{self.nome} acelera com tudo!!","green"))
             self.pos += 2    
         elif self.pos == 13:
-            print(self.nome,"Bateu o carro e terá que voltar para o inicio!!")
+            print(colored(f"{self.nome} bateu o carro e terá que voltar para o inicio!!","white","on_red"))
             self.pos = 0
+        if self.pos < 20:
+            print("Posição de",self.nome,':',self.pos)
+        if self.pos >= 20:
+            print(colored(f"{self.nome} passa pela linha de chegada na posição: {self.pos}","yellow"))
 
-            
     def getPos(self):
         return self.pos
     
 dadovirtual1 = dado(6)
-competidor1 = Competidor("Gabriel")
-competidor2 = Competidor("Heitor")
-competidor3 = Competidor("Rafael")
-competidor4 = Competidor("Lucas")
-competidor5 = Competidor("Matheus")
-competidor6 = Competidor("Guilherme")
+total = False
+Competidores = []
+
+os.system('cls' if os.name == 'nt' else 'clear')
+print(colored("CORRIDA MALUCA","cyan", attrs=["bold","underline"]))
+input("Entrar...")
+os.system('cls' if os.name == 'nt' else 'clear')
+
+while total != True:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    Competidores.append(Competidor(input("Nome do competidor: ")))
+    conc = input("Concluir? (S/N): ")
+    if conc == "S" or conc == "s":
+        total = True
+    else:
+        pass
+
 vencedor = False
 rodada = 1
 while vencedor != True:
     os.system('cls' if os.name == 'nt' else 'clear')
     print("Rodada:",rodada)
-    competidor1.atualizar(dadovirtual1.rolar())
-    competidor2.atualizar(dadovirtual1.rolar())
-    competidor3.atualizar(dadovirtual1.rolar())
-    competidor4.atualizar(dadovirtual1.rolar())
-    competidor5.atualizar(dadovirtual1.rolar())
-    competidor6.atualizar(dadovirtual1.rolar())
-    posicaocomp1 = competidor1.getPos()
-    posicaocomp2 = competidor2.getPos()
-    posicaocomp3 = competidor3.getPos()
-    posicaocomp4 = competidor4.getPos()
-    posicaocomp5 = competidor5.getPos()
-    posicaocomp6 = competidor6.getPos()
-    if posicaocomp1 >= 20:
-        print("Gabriel passa pela linha de chegada na posição:",posicaocomp1)
-        vencedor = True
-    if posicaocomp1 < 20:
-        print("Posição de Gabriel:",posicaocomp1)
-    if posicaocomp2 >= 20:
-        print("Heitor passa pela linha de chegada na posição:",posicaocomp2)
-        vencedor = True
-    if posicaocomp2 < 20:
-        print("Posição de Heitor:",posicaocomp2)
-    if posicaocomp3 >= 20:
-        print("Rafael passa pela linha de chegada na posição:",posicaocomp3)
-        vencedor = True
-    if posicaocomp3 < 20:
-        print("Posição de Rafael:",posicaocomp3)
-    if posicaocomp4 >= 20:
-        print("Lucas passa pela linha de chegada na posição:", posicaocomp4)
-        vencedor = True
-    if posicaocomp4 < 20:
-        print("Posição de Lucas:",posicaocomp4)
-    if posicaocomp5 >= 20:
-        print("Matheus passa pela linha de chegada na posição:", posicaocomp5)
-        vencedor = True
-    if posicaocomp5 < 20:
-        print("Posição de Matheus:",posicaocomp5)
-    if posicaocomp6 >= 20:
-        print("Guilherme passa pela linha de chegada na posição:", posicaocomp6)  
-        vencedor = True
-    if posicaocomp6 < 20:
-        print("Posição de Guilherme:",posicaocomp6)
+    for competidor in Competidores:
+        competidor.atualizar(dadovirtual1.rolar())   
+        pos = competidor.getPos()
+        if pos >= 20:
+            vencedor = True
     input("Continuar...")
     rodada += 1  
